47a43062637ea300dbc0bdde16e7c3982abdfc82
Sydonae England <sydonaeengland@gmail.com>
2025-03-16 23:27:12 -0500

    Completed Exercise 5

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired
-
+from flask_wtf.file import FileAllowed
+from wtforms import FileField, SubmitField
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    file = FileField("Upload File", validators=[
+        InputRequired(),
+        FileAllowed(['jpg', 'png'], "Only JPG and PNG images are allowed!")  
+    ])
+    submit = SubmitField("Upload")
--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
 from app.models import UserProfile
 from app.forms import LoginForm
-
+from app.forms import UploadForm
 
 ###
 # Routing for your application.
@@ -25,17 +25,22 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required 
 def upload():
     # Instantiate your form class
+    form = UploadForm()
 
-    # Validate file upload on submit
+   
+     # Validate file upload on submit
     if form.validate_on_submit():
-        # Get file data and save to your uploads folder
-
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        print(f"File received: {filename}")
+        print(f"Saving to: {os.path.join(app.config['UPLOAD_FOLDER'], filename)}")
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
-
-    return render_template('upload.html')
+    return render_template('upload.html', form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
